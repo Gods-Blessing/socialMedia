7972bb7eb12b1b522590e6168b266d0d9b5402a5
Siddhant sharma <hitechsidu992@gmail.com>
2023-03-13 23:58:10 +0530

    Modify Signin Page

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const port = 2002;
 const app = express();
 
+const Ejslayout= require('express-ejs-layouts');
+
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.set("layout extractStyles", true)
+app.set("layout extractScripts", true)
 app.use(express.static('assets'));
-
+app.use(Ejslayout);
 app.use('/', require('./routes/index') );
 
 
